ab5253ebedf1d5292d348d4ce5e71e79de059752
ezittel <ezittel@nycap.rr.com>
2020-10-02 15:49:23 -0400

    Updates

---
+++ precache-manifest.dcba5c8a79f35c615cd6644688fc07c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd41a35d1a5a9c8a3d190adaff4d9f24",
+    "url": "/simple-todo/index.html"
+  },
+  {
+    "revision": "3c10b694197d34555b5f",
+    "url": "/simple-todo/static/css/main.ebf3308a.chunk.css"
+  },
+  {
+    "revision": "eef57d1a8ae693463c88",
+    "url": "/simple-todo/static/js/2.37592422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo/static/js/2.37592422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c10b694197d34555b5f",
+    "url": "/simple-todo/static/js/main.998483ee.chunk.js"
+  },
+  {
+    "revision": "a9b828f3067ebb10d15b",
+    "url": "/simple-todo/static/js/runtime-main.b107b839.js"
+  }
+]);
